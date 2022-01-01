ff6f86e4163e839e0ef8fd324dbccdedb09d8047
Martin Stickley <mds1966@gmail.com>
2022-01-01 22:14:01 +0000

    Indroduced randomised arcs with scale, angle and radius. Also increased the slice values and inverted white and black

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,25 +1,17 @@
 
-const canvasSketch = require('canvas-sketch')
-const math = require('canvas-sketch-util/math')
-const random = require('canvas-sketch-util/random')
+const canvasSketch  = require('canvas-sketch')
+const math          = require('canvas-sketch-util/math')
+const random        = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-// const degToRad = (degrees) => {
-//   return degrees / 180 * Math.PI
-// }
-// 
-// const randomRange = (min, max) => {
-//   return Math.random() * (max - min) + min
-// }
-
 const sketch = () => {
 
   return ({ context, width, height }) => {
 
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = 'black'
@@ -33,7 +25,7 @@ const sketch = () => {
     const h = height * 0.1
     let x, y
 
-    const num = 12
+    const num = 40
     const radius = width * 0.3
 
     for (let i = 0; i < num; i++) {
@@ -45,22 +37,27 @@ const sketch = () => {
       y = radius * Math.cos(angle)
 
       context.save()
-      context.translate(cx, cy)
-      context.translate(x, y)
+        context.fillStyle = 'white'
+        context.translate(cx, cy)
+        context.translate(x, y)
+        context.rotate(-angle)
+        context.scale(random.range(0.1, 2), random.range(0.2, 0.5))
+        context.beginPath()
+        context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h)
+        context.fill()
+      context.restore()
 
-      // Draw a small red circle right here
-      context.fillStyle = 'red'
-      context.beginPath()
-      context.arc(0, 0, 20, 0, Math.PI * 2)
-      context.fill()
-
-      // Draw rectangle after adjusting the angle
-      context.fillStyle = 'black'
-      context.rotate(-angle)
-      context.scale(random.range(1, 3), random.range(1, 3))
-      context.beginPath()
-      context.rect(-w * 0.5, -h * 0.5, w, h)
-      context.fill()
+      context.save()
+        context.strokeStyle = 'white'
+        context.translate(cx, cy)
+        context.rotate(-angle)
+        context.lineWidth = random.range(5, 20)
+        context.beginPath()
+        context.arc(0, 0, radius * random.range(0.7, 1.3), 
+          slice * random.range(0, -8), 
+          slice * random.range(1, 5)
+        )
+        context.stroke()
       context.restore()
     }
 
