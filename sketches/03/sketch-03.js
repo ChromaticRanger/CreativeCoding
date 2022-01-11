8ad02833a01dd6bf00afa7e65b5c2ca8b3c69a98
Martin Stickley <mds1966@gmail.com>
2022-01-11 22:29:49 +0000

    Added animation to the agents

--- sketches/03/sketch-03.js
+++ sketches/03/sketch-03.js
@@ -3,7 +3,8 @@ const canvasSketch = require('canvas-sketch')
 const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
 const sketch = ({ context, width, height }) => {
@@ -17,11 +18,13 @@ const sketch = ({ context, width, height }) => {
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'lightgrey';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     agents.forEach(agent => {
+      agent.update()
       agent.draw(context)
+      agent.bounce(width, height)
     })
 
   };
@@ -29,7 +32,7 @@ const sketch = ({ context, width, height }) => {
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x
     this.y = y
@@ -37,15 +40,40 @@ class Point {
 }
 
 class Agent {
+
   constructor(x, y) {
-    this.pos = new Point(x, y)
-    this.radius = 10
+    this.pos = new Vector(x, y)
+    this.vel = new Vector(random.range(-3, 3), random.range(-3, 3))
+    this.radius = random.range(4, 12)
+  }
+
+  bounce(width, height) {
+    if( this.pos.x <= 0 + this.radius || this.pos.x + this.radius >= width ) {
+      this.vel.x *= -1
+    }
+
+    if(this.pos.y <= 0 + this.radius || this.pos.y + this.radius >= height ) {
+      this.vel.y *= -1
+    }
+  }
+
+  update() {
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y
   }
 
   draw(context) {
-    context.fillStyle = 'black'
+
+    context.save()
+    context.translate(this.pos.x, this.pos.y)
+    context.lineWidth = 4
+
     context.beginPath()
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2)
+    context.arc(0, 0, this.radius, 0, Math.PI * 2)
     context.fill()
+    context.stroke()
+
+    context.restore()
   }
+
 }
